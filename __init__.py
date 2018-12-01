3d739b562d78d1f5e2ef09464f9ff4845d7d9993
Yilu Zeng <zengyl.yolanda2015@outlook.com>
2018-11-30 23:02:04 -0500

    corrected approute

--- __init__.py
+++ __init__.py
@@ -2,15 +2,18 @@
 def page_not_found(e):
     return redirect('/')
 
-
 @app.route('/home', methods=['GET'])
 def home():
     return render_template('home/home.html')
 
+@app.route('/car_info', methods=['GET'])
+def car_info():
+    return render_template('car_info/car_info.html')
 
-@app.route('/car-info', methods=['GET'])
-def car-info():
-    return render_template('car-info/car-info.html')
+@app.route('/car_form', methods=['GET'])
+def car_form():
+    return render_template('car_form/car_form.html')
 
-@app.route('/car-form', methods=['GET'])
-def
+@app.route('/control_panel', methods=['GET'])
+def car_panel():
+    return render_template('car_panel/car_panel.html')
